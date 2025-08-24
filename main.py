0595f3d9c1da01de120bcb183f0d402b594b0c09
Kevin <marinkevin159@gmail.com>
2025-08-23 22:59:42 -0500

    Proyecto a con sus funcionalidades terminadas, falta darle más enfoque en otras areas

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import json
-import os
 
 
 ID = 1
 tasks = []
 
-def menu():
-    print(" ---------------- Menu: ----------------\n"
-    "1. Añadir nueva tarea \n"
-    "2. Buscar tarea por ID \n"
-    "3. Listar todas las tareas \n"
-    "4. Eliminar tarea por ID \n"
-    "--------------------------------------")
-    choice = int(input("Seleccione una opción (Digitando el número): "))
-    return choice
+
 
 
 def add_task():
@@ -45,9 +36,9 @@ def search_task():
 
     for task in tasks:
         if task["ID"] == search_ID:
-            print(f"Tarea encontrada: ID: {task['ID']}, Tarea: {task['Task']}, Descripción: {task['Description']}")
+            print(f"Tarea encontrada: ID: {task['ID']}, Tarea: {task['Task']}, Descripción: {task['Description']} \n")
         else:
-            print("Tarea no encontrada.")
+            print("Tarea no encontrada. \n")
 
 def list_tasks():
 
@@ -55,20 +46,41 @@ def list_tasks():
         tasks = json.load(file)
     
     for task in tasks:
-        print(f"ID: {task['ID']}, Tarea: {task['Task']}, Descripción: {task['Description']}")
-        
+        print(f"ID: {task['ID']}, Tarea: {task['Task']}, Descripción: {task['Description']} \n")
+
 
 
 def delete_task():
 
-    ...
+    delete = int(input("Ingrese el ID de la tarea que desea eliminar: "))
 
+    with open("task.json", "r") as file:
+        tasks = json.load(file)
+    for task in tasks:
+        if task["ID"] == delete:
+            tasks.remove(task)
+            with open("task.json", "w") as file:
+                json.dump(tasks, file, indent=4)
+            print(f"Tarea con ID {delete} eliminada correctamente. \n")
+            break
+    else:
+        print("Tarea no encontrada. \n")
   
   
   
 
 while True:
-    menu()
+    def menu():
+        print(" ---------------- Menu: ----------------\n"
+        "1. Añadir nueva tarea \n"
+        "2. Buscar tarea por ID \n"
+        "3. Listar todas las tareas \n"
+        "4. Eliminar tarea por ID \n"
+        "5. Salir \n"
+        "--------------------------------------")
+        choice = int(input("Seleccione una opción (Digitando el número): "))
+        return choice    
+
     choice = menu()
     if choice == 1:
         add_task()
@@ -78,6 +90,8 @@ while True:
         list_tasks()
     elif choice == 4:
         delete_task()
+    else: 
+        print("Saliendo del programa...")
 
     continuar = input("¿Desea agregar otra tarea? (s/n): ").lower()
     if continuar != 'n':
